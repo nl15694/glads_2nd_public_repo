b3bfec3e9bff7dc40e6365e634030a005a2d59fd
Linux User <fromthechurchy@gmail.com>
2020-06-29 21:20:25 +0000

    Add Spring Boot skeleton

---
+++ glads_2nd_public_repo/src/main/java/com/nl15694/glads_2nd_public_repo/Glads2ndPublicRepoApplication.java
@@ -0,0 +1,13 @@
+package com.nl15694.glads_2nd_public_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glads2ndPublicRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glads2ndPublicRepoApplication.class, args);
+	}
+
+}
---
+++ glads_2nd_public_repo/src/test/java/com/nl15694/glads_2nd_public_repo/Glads2ndPublicRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nl15694.glads_2nd_public_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glads2ndPublicRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
